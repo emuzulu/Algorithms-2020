523b4d20c31735d300d8ff6975648ab07bab51bc
KRNhikari <34624141+StardustQVQ@users.noreply.github.com>
2020-02-18 00:36:01 -0500

    add more explaination

--- Get Rec'd(in Homework2)/Main.java
+++ Get Rec'd(in Homework2)/Main.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	static int[] d = new int[5100];
+	static int[] d = new int[5100];//the maximum length for a start vertex
 	static int[] a = new int[5100];//width of rec
 	static int[] b = new int[5100];//length of rec
 	static int[][] G = new int[5100][5100];//the DAG
 	static int  n = 0;//number of rec
-	static int dp(int i) {
+	static int dp(int i) {//to get the d(i)
 		if(d[i] > 0)//if the answer have already
 			return d[i];
 		d[i] = 1;//initialize the max length to 1
 		for(int j = 1; j <= n; j++) {
 			if(G[i][j] == 1)
-				d[i] = Math.max(d[i], dp(j)+1);//get the maximum length at i recursively 
+				d[i] = Math.max(d[i], dp(j)+1);//get d(i) recursively by the formula 
 		}
 		return d[i];//return the maximum lenth at i
 	}
@@ -26,14 +26,14 @@ public class Main {
 		}
 		for(int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				if ((a[i] < a[j] && b[i] < b[j]) || (b[i] < a[j] && a[i] < b[j]))
+				if ((a[i] < a[j] && b[i] < b[j]) || (b[i] < a[j] && a[i] < b[j]))//if i can nest in j
 					G[i][j] = 1;// form the DAG
 			}
 		}
 		int maxi = 0;
 		for(int i = 1; i <= n; i++) {
-			if(dp(maxi) < dp(i)) {
-				maxi = i;
+			if(dp(maxi) < dp(i)) {//if there is a longer length start from i
+				maxi = i;//change the index
 			}
 		}
 		System.out.println(d[maxi]);//output the answer
